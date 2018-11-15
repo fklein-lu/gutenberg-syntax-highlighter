99606b7bf5148633dfc7de3eb094f3c3167ed103
Fränk Klein <frank@humanmade.co.uk>
2018-11-15 18:37:27 +0100

    Add main plugin file with headers.

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Gutenberg Syntax Highlighter
+ * Plugin URI: https://wpdevelopment.courses/plugins/gutenberg-syntax-highlighter/
+ * Description: Extends Gutenberg's code block to add optional syntax highlighting.
+ * Author: Fränk Klein
+ * Author URI: https://wpdevelopment.courses/
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ */
